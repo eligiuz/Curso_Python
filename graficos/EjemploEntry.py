e83c159c3a23af03743b918eeddfb58008b5d451
Eligio Cachón Menéndez <ecachon@gmail.com>
2018-08-03 16:27:37 -0500

    Ejemplos de entradas de texto. Inicio de calculadora
    Terminación de las entradas de texto y comienzo de la calculadora

--- graficos/EjemploEntry.py
+++ graficos/EjemploEntry.py
@@ -6,8 +6,10 @@ raiz = Tk()
 miFrame = Frame(raiz, width=1200, heigh=600)
 miFrame.pack()
 
+minombre = StringVar()
+
 # Asignamos  auna variable el cuadro de texto que estara dentro del frame
-cuadroNombre = Entry(miFrame)
+cuadroNombre = Entry(miFrame, textvariable=minombre)
 cuadroNombre.grid(row=0, column=1, padx=10, pady=10)
 cuadroNombre.config(fg="red", justify="center")
 
@@ -20,6 +22,17 @@ cuadroApellido.grid(row=2, column=1, padx=10, pady=10)
 
 cuadroDireccion = Entry(miFrame)
 cuadroDireccion.grid(row=3, column=1, padx=10, pady=10)
+# Agregamos una ventana de texto con Text() l eponemos que pertenece a un frame y le damos un tamaño dentrod e ese frame.
+textoComentario = Text(miFrame, width=16, height=5)
+textoComentario.grid(row=4, column=1, padx=10, pady=10)
+# --------  USO DE SCROLLBAR
+# Para agregar el scroll bar se le asigna a una variable la función Scrollbar() diciendo a quien pertenece  y con command() se le dice en donde debe de estar y com se movera x horizontal y vertical
+scrollVert = Scrollbar(miFrame, command=textoComentario.yview)
+# Se coloca sticky para adaptar al tamaño de la ventana de texto
+scrollVert.grid(row=4, column=2, sticky="nsew")
+# Utilizo config() y yscrollcommand(nombredelscroll.set) para controlar el movimiento del cursos
+textoComentario.config(yscrollcommand=scrollVert.set)
+# --------  USO DE SCROLLBAR
 
 # sticky alinea el texto a la orinetaión que le mencione. El padx o pady se utiliza para dar padding
 nombreLabel = Label(miFrame, text="Nombre:")
@@ -28,10 +41,27 @@ nombreLabel.grid(row=0, column=0, sticky="e", padx=10, pady=10)
 apellidoLabel = Label(miFrame, text="Apellido:")
 apellidoLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
 
-direccionLabel = Label(miFrame, text="Direccion de casa:")
+direccionLabel = Label(miFrame, text="Direccion:")
 direccionLabel.grid(row=3, column=0, sticky="e", padx=10, pady=10)
 
 passLabel = Label(miFrame, text="Password:")
 passLabel.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+comentariosLabel = Label(miFrame, text="Comentarios:")
+comentariosLabel.grid(row=4, column=0, sticky="e", padx=10, pady=10)
+
+# ------ AGREGAR BOTON
+
+# Funcion que unicamente asigna nuestro nombre a una variable que sera desplegada en la caja de nombre
+def codigoBoton():
+
+    minombre.set("Eligio")
+
+
+# Se crea el boton y con command se le dice que debe estar asociado a la funcion codigoboton
+botonEnvio = Button(raiz, text="Enviar", command=codigoBoton)
+
+botonEnvio.pack()
+
 
 raiz.mainloop()
